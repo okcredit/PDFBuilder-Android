8fb7d49d1bcf25684a874a831934601ad648e082
anjalsaneen <43947967+anjalsaneen@users.noreply.github.com>
2023-06-19 16:00:13 +0530

    Initial Commit. Created an App Module.

---
+++ app/src/main/java/tech/okcredit/pdfbuilder_android/MainActivity.kt
@@ -0,0 +1,10 @@
+package tech.okcredit.pdfbuilder_android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
